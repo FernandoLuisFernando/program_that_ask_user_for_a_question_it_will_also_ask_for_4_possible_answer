c6bb6bd6d51bc9e8fee9c2f6a6661b9ee418567d
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-04-09 11:56:18 +0800

    spacing_edited

--- Quiz.py
+++ Quiz.py
@@ -3,23 +3,23 @@ with open("questions.txt", "w") as file:
         print(f"\n--- Question {quests} ---")
         question = input("Enter the question: ")
 
-option_a = input("Enter option a: ")
-option_b = input("Enter option b: ")
-option_c = input("Enter option c: ")
-option_d = input("Enter option d: ")
+        option_a = input("Enter option a: ")
+        option_b = input("Enter option b: ")
+        option_c = input("Enter option c: ")
+        option_d = input("Enter option d: ")
 
-correct_answer = ""
-while correct_answer not in ["a", "b", "c", "d"]:
-    correct_answer = input ("Enter the correct answer (a/b/c/d): ").lower()
-    if correct_answer not in ["a", "b", "c", "d"]:
-        print("Please enter a valid choice (a, b ,c , or d).")
+        correct_answer = ""
+        while correct_answer not in ["a", "b", "c", "d"]:
+            correct_answer = input ("Enter the correct answer (a/b/c/d): ").lower()
+            if correct_answer not in ["a", "b", "c", "d"]:
+                print("Please enter a valid choice (a, b ,c , or d).")
 
-file.write(f"Question {quests}: {question}\n")
-file.write(f"a) {option_a}\n")
-file.write(f"b) {option_b}\n")
-file.write(f"d) {option_c}\n")
-file.write(f"c) {option_d}\n")
-file.write(f"Correct Answer: {correct_answer}\n")
-file.write("---\n")
+        file.write(f"Question {quests}: {question}\n")
+        file.write(f"a) {option_a}\n")
+        file.write(f"b) {option_b}\n")
+        file.write(f"d) {option_c}\n")
+        file.write(f"c) {option_d}\n")
+        file.write(f"Correct Answer: {correct_answer}\n")
+        file.write("---\n")
 
 print("\nAll questions have been saved to 'questions.txt'.")
