ae1b89255e4ea9b484d921f66ba9a98ee0eee10a
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-04-09 11:36:28 +0800

    creating_multiple_choices_for_user_input

--- Quiz.py
+++ Quiz.py
@@ -1,3 +1,8 @@
 with open("questions.txt", "w") as file:
     for questions in range (1, 4):
-        
+        print(f"\n--- Question {questions} ---")
+queston = input("Enter option a: ")
+queston = input("Enter option b: ")
+queston = input("Enter option c: ")
+queston = input("Enter option d: ")
+
