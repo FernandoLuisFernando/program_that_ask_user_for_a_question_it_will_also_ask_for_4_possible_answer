047f2f2165cc0e16e966263ab094bca8d329e21a
Fernando Luis R. Fernando <fernandoluisr.fernando28@gmail.com>
2025-04-09 11:43:35 +0800

    asking_user_for_correct_answer

--- Quiz.py
+++ Quiz.py
@@ -6,3 +6,8 @@ queston = input("Enter option b: ")
 queston = input("Enter option c: ")
 queston = input("Enter option d: ")
 
+correct_answer = ""
+while correct_answer not in ["a", "b", "c", "d"]:
+    correct_answer = input ("Enter the correct answer (a/b/c/d): ").lower()
+    if correct_answer not in ["a", "b", "c", "d"]:
+        print("Please enter a valid choice (a, b ,c , or d).")
